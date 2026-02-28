424dd301ee98e20f690d940d21bf3dfb3ae5b0ca
Emil Gibadullin <gibadullin.emil@yandex.ru>
2026-02-28 15:39:09 +0300

    fixed: - incorrect path in services.ts for typeOfServices request

--- lib/api/services.ts
+++ lib/api/services.ts
@@ -21,5 +21,5 @@ export async function getServiceTypes(
     size: size.toString(),
   });
 
-  return client.get<ServiceTypesResponse>(`/typeOfService?${params.toString()}`);
+  return client.get<ServiceTypesResponse>(`/typeOfServices?${params.toString()}`);
 }
